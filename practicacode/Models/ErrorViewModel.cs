177ce69f1b6eb9ae15f01dd1c0559b25c2130ee8
bayron <bayronforces69@gmail.comI>
2025-03-10 10:26:44 -0600

    actu practicacode

---
+++ practicacode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practicacode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
